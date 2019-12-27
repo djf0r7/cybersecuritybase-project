e502d4d9eb78daba5ae57b08c34db7f5301550ad
djf0r7 <27811609+djf0r7@users.noreply.github.com>
2019-12-27 16:19:05 +0200

    added some security / flaws

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,9 +20,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // no real security at the moment
+        http.csrf().disable();
+        http.headers().disable();
+        http.formLogin().permitAll();
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .antMatchers("/h2-console/*").permitAll()
+                .anyRequest().authenticated();
     }
 
     @Autowired
